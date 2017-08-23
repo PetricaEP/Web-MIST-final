91ff8916e877c2933c2c7ea3fbc535b959579d93
José Augusto Salim <jose@Joses-MacBook-Air.local>
2017-08-23 15:35:09 -0300

    x, y

--- ep-db/scripts/sinteticDataGenerator.sh
+++ ep-db/scripts/sinteticDataGenerator.sh
@@ -2,10 +2,14 @@
 
 max_depth=$1
 number_of_points=$2
-output_file=$3
+x=$3
+y=$4
+output_file=$5
 
 echo $max_depth
 echo $number_of_points
+echo $x
+echo $y
 echo $output_file
 
-sbt "runMain ep.db.utils.SinteticDataGenerator ${max_depth} ${number_of_points} ${output_file}" 
+sbt "runMain ep.db.utils.SinteticDataGenerator ${max_depth} ${number_of_points} ${x} ${y} ${output_file}" 
